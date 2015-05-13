0e0a051d141492ca4d32b665b48bebe29574fcce
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-05-13 23:59:46 +0900

    add way to get resource_token

---
+++ auth.js
@@ -0,0 +1,4 @@
+var auth = (function (){
+  var client_id = "c57c4ee639eb5922fff55a592c74ebf99845a5a13e8f4f0b87b8ac251a88aa89";
+  var client_secret = "4f95408f81887be0b32e24fdd2bbc5d05db159b1343f4020337e4516e79f5cc4";
+}());
--- background.js
+++ background.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 (function(){
+  console.log("hogehoge");
   $.ajaxSetup({
       headers: {
         'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
@@ -14,14 +15,39 @@
 //  console.log("onChanged" + JSON.stringify(info));
 //});
 
-chrome.tabs.onCreated.addListener(function(tabId, info, tab){
-  
+chrome.tabs.onCreated.addListener(function(tab){
+  var resource_token;
+  var authorization;
+  var _res;
+
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:3000/oauth/token?grant_type=password&username=r-fujiwara@nekojarashi.com&password=ne5suke38&client_id=c57c4ee639eb5922fff55a592c74ebf99845a5a13e8f4f0b87b8ac251a88aa89&client_secret=4f95408f81887be0b32e24fdd2bbc5d05db159b1343f4020337e4516e79f5cc4"
+  }).done(function(res){
+      _res = res;
+      console.log(res);
+      resource_token = res.access_token
+  });
+  authorization = "Bearer " + resource_token
+  console.log("tab_url..." + tab.url);
+
+/*
+  if(tab.url.indexOf("http://stackoverflow.com/questions/") < 0){
+    return
+  }
+*/
 
   $.ajax({
     method: "GET",
+    beforeSend: function(xhr){
+      debugger
+      xhr.setRequestHeader("Authorization", authorization);
+    },
     url: "http://localhost:3000/posts.json"
   }).done(function(res){
-    console.log("reponse..." + res);
+    res.map(function(r){
+      console.log("title..." + r.title);
+    });
   });
 
   /**
