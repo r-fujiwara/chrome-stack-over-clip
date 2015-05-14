42a4bc27497d5f3f10c55abbb8e69060e321bc76
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-05-14 16:08:32 +0900

    notify duplicate stackoverflow content

--- background.js
+++ background.js
@@ -9,16 +9,21 @@
         'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
       }
   });
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+
 })();
 
 //chrome.cookies.onChanged.addListener(function(info) {
 //  console.log("onChanged" + JSON.stringify(info));
 //});
 
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 chrome.tabs.onCreated.addListener(function(tab){
   var resource_token;
   var authorization;
 
+  /**
   $.ajax({
     method: "POST",
     url: "http://localhost:3000/oauth/token?grant_type=password&username=r-fujiwara@nekojarashi.com&password=ne5suke38&client_id=c57c4ee639eb5922fff55a592c74ebf99845a5a13e8f4f0b87b8ac251a88aa89&client_secret=4f95408f81887be0b32e24fdd2bbc5d05db159b1343f4020337e4516e79f5cc4"
@@ -27,6 +32,7 @@ chrome.tabs.onCreated.addListener(function(tab){
     resource_token = res.access_token
 
     authorization = "Bearer " + resource_token
+
     $.ajax({
       method: "GET",
       beforeSend: function(xhr){
@@ -39,30 +45,29 @@ chrome.tabs.onCreated.addListener(function(tab){
       });
     });
   });
+  **/
 
-/*
   if(tab.url.indexOf("http://stackoverflow.com/questions/") < 0){
     return
   }
-*/
+  debugger
 
-
-  /**
   $.ajax({
     method: "POST",
     url: "http://localhost:3000/posts.json",
     data:{
       post: {
-        title: "hogehoge33333333",
-        url: "htttp://fugafuga.com/hogehoge.com",
+        title: tab.title,
+        url: tab.url,
         content: "someone!",
-        user_id: "1"
+        user_id: 1
       }
     }
   }).done(function(res){
-    console.log("reponse..." + res);
+    if(res.refer_recent){
+      notifyAlert();
+    }
   });
-  **/
 
   /**
   var xhr = new XMLHttpRequest();
@@ -75,6 +80,16 @@ chrome.tabs.onCreated.addListener(function(tab){
   }
   **/
 });
+
+var notifyAlert = function(){
+  var  imgNumber = Math.floor(Math.random()*(6 - 0));
+  var  icon_path = "img/" + imgNumber + ".jpg"
+  var notification = new Notification('吹留名言集', {
+    lang: "EUC-JP",
+    icon: icon_path,
+    body: "いやぁ↑、覚えるだけっていうのは誰でも出来ると思うんですよぉ↑"
+  });
+}
 
 function focusOrCreateTab(url) {
   chrome.windows.getAll({"populate":true}, function(windows) {
