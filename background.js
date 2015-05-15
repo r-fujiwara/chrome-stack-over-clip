9293fde587b15e16e344339668a9ac8d5cdc442e
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-05-15 11:35:40 +0900

    add login feature

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
 
 (function(){
-  console.log("hogehoge");
+  var LOGIN_URL = "chrome-extension://" + chrome.runtime.id + "/manager.html"
+
   $.ajaxSetup({
       headers: {
         'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
@@ -12,49 +14,69 @@
   if (Notification.permission !== "granted")
     Notification.requestPermission();
 
+  var initialSetup =  function(){
+    if((localStorage.getItem("login") == undefined) & (localStorage.getItem("password") == undefined)){
+      chrome.tabs.getAllInWindow(function(tabs){
+        var urls = _.map(tabs, function(tab){ return tab.url})
+        if(!(_.contains(urls, LOGIN_URL))){
+          return window.open(LOGIN_URL)
+        }
+      })
+    }
+  }
+  initialSetup()
+
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if((request.login == localStorage.getItem("login")) && (request.password == localStorage.getItem("password"))){
+        sendResponse({msg: "get_lucky"});
+      }else{
+        sendResponse({msg: "お前の席ねーから!"});
+      }
+    }
+  )
 })();
 
-//chrome.cookies.onChanged.addListener(function(info) {
-//  console.log("onChanged" + JSON.stringify(info));
-//});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  console.log("url....", tab.url);
 
-//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-chrome.tabs.onCreated.addListener(function(tab){
+  console.log("status" + changeInfo.status);
+
+  if(changeInfo.status != "complete")
+    return;
+
+  console.log("complete")
   var resource_token;
   var authorization;
+  var username = localStorage.getItem("login")
+  var password = localStorage.getItem("password")
+  var client_id = "f935d2d9325dbf8a1ad8a6b4298fd3d306f308e7a2868e01e2281c0ce4d3f179"
+  var client_secret = "e22c1f307d524761a7357f36e274adc57d1df326c2a807051e065284d7bd8fcd"
+  var url = "http://api.localhost:3000/oauth/token?grant_type=password&username=" + username + "&password=" + password + "&client_id=" + client_id + "&client_secret=" + client_secret
 
-  /**
   $.ajax({
     method: "POST",
-    url: "http://localhost:3000/oauth/token?grant_type=password&username=r-fujiwara@nekojarashi.com&password=ne5suke38&client_id=c57c4ee639eb5922fff55a592c74ebf99845a5a13e8f4f0b87b8ac251a88aa89&client_secret=4f95408f81887be0b32e24fdd2bbc5d05db159b1343f4020337e4516e79f5cc4"
+    url: url
   }).done(function(res){
 
-    resource_token = res.access_token
+    resource_token = res.access_token;
 
-    authorization = "Bearer " + resource_token
-
-    $.ajax({
-      method: "GET",
-      beforeSend: function(xhr){
-        xhr.setRequestHeader("Authorization", authorization);
-      },
-      url: "http://localhost:3000/posts.json"
-    }).done(function(res){
-      res.map(function(r){
-        console.log("title..." + r.title);
-      });
-    });
+    authorization = "Bearer " + resource_token;
+    console.log("request with token")
+    resourceTokenCallBack(tab, authorization);
   });
-  **/
+});
 
+var resourceTokenCallBack = function(tab, auth_token){
   if(tab.url.indexOf("http://stackoverflow.com/questions/") < 0){
     return
   }
-  debugger
 
   $.ajax({
     method: "POST",
-    url: "http://localhost:3000/posts.json",
+    url: "http://api.localhost:3000/posts.json",
+    beforeSend: function(xhr){
+      xhr.setRequestHeader("Authorization", auth_token);
+    },
     data:{
       post: {
         title: tab.title,
@@ -68,51 +90,14 @@ chrome.tabs.onCreated.addListener(function(tab){
       notifyAlert();
     }
   });
-
-  /**
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://localhost:3000/posts.json", true);
-  xhr.onreadystatechange = function(){
-    console.log("ready state")
-    if (xhr.readyState == 4) {
-      console.log("resp...." +  xhr.responseText);
-    }
-  }
-  **/
-});
+}
 
 var notifyAlert = function(){
   var  imgNumber = Math.floor(Math.random()*(6 - 0));
   var  icon_path = "img/" + imgNumber + ".jpg"
-  var notification = new Notification('吹留名言集', {
+  var notification = new Notification('街コン速報', {
     lang: "EUC-JP",
     icon: icon_path,
-    body: "いやぁ↑、覚えるだけっていうのは誰でも出来ると思うんですよぉ↑"
+    body: "5/22(金)は、街コン(青山)"
   });
 }
-
-function focusOrCreateTab(url) {
-  chrome.windows.getAll({"populate":true}, function(windows) {
-    var existing_tab = null;
-    for (var i in windows) {
-      var tabs = windows[i].tabs;
-      for (var j in tabs) {
-        var tab = tabs[j];
-        if (tab.url == url) {
-          existing_tab = tab;
-          break;
-        }
-      }
-    }
-    if (existing_tab) {
-      chrome.tabs.update(existing_tab.id, {"selected":true});
-    } else {
-      chrome.tabs.create({"url":url, "selected":true});
-    }
-  });
-}
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var manager_url = chrome.extension.getURL("manager.html");
-  focusOrCreateTab(manager_url);
-});
